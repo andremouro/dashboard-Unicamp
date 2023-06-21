c5964438e5610a550d279a5337bfdd0b85e4f6b6
andremouro <andremourodangioli@gmail.com>
2023-06-21 11:20:38 -0300

    exporting the first .csv file

--- Dados/DataScript230621.py
+++ Dados/DataScript230621.py
@@ -12,3 +12,7 @@ df_esp['UNIDADE'] = df_esp['UNIDADE'].str.replace("[ ]{2,}","",regex=True)
 #adicionamos a matricula 00000 para alunos e professores não cadastrados corretamente
 df_esp['MATRICULA RA'] = df_esp['MATRICULA RA'].str.replace("[ ]{2,}","00000",regex=True)
 
+#exportar o data frame normalizado como csv. Este é o primeiro df que será usado para quantificar 
+#os alunos e professores por disciplina, etc.
+
+df_esp.to_csv('espelho_comp.csv', encoding = 'latin-1', sep=';', index=False)
