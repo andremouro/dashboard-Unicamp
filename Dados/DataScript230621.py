7415d6ff642cb7756dc6c1e368419aec22046430
andremouro <andremourodangioli@gmail.com>
2023-06-21 11:09:42 -0300

    adding the ra normalization for unregistered students and teachers

--- Dados/DataScript230621.py
+++ Dados/DataScript230621.py
@@ -3,12 +3,12 @@ import numpy as np
 import os
 import math
 
-
 #ler cada um dos bancos de dados
 df_esp = pd.read_csv('DASH.eaespelho.todos.csv',  sep=';', encoding = 'latin-1', dtype='unicode')
 
 #retiramos todos os espaços que podem aparecer ao final do nome da unidade
 df_esp['UNIDADE'] = df_esp['UNIDADE'].str.replace("[ ]{2,}","",regex=True)
 
-print(df_esp.head())
+#adicionamos a matricula 00000 para alunos e professores não cadastrados corretamente
+df_esp['MATRICULA RA'] = df_esp['MATRICULA RA'].str.replace("[ ]{2,}","00000",regex=True)
 
