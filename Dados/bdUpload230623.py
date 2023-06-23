884d9b5792e42cb1fb3a13689131b3a7ce74fc68
andremouro <andremourodangioli@gmail.com>
2023-06-23 10:56:11 -0300

    adding the script to upload data to DB

---
+++ Dados/bdUpload230623.py
@@ -0,0 +1,25 @@
+import mysql.connector
+from mysql.connector import errorcode
+import pandas as pd
+
+
+#importando o csv com os dados
+df_comp = pd.read_csv('espelho_comp.csv',  sep=';', encoding='latin-1')
+
+
+cnx = mysql.connector.connect(user = 'root', password = '', host = 'localhost', database = 'mydb')
+
+cursor = cnx.cursor()
+
+
+for i in range(0, len(df_comp['id'])):
+    add_entry = ("INSERT INTO dashboard_dacmood (id, ra, nome_curto, papel, instituicao, nivel, unidade, sigla_uni) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)")
+    data_entry = (int(df_comp['id'][i]), str(df_comp['ra'][i]), str(df_comp['nome_curto'][i]), str(df_comp['papel'][i]), str(df_comp['instituicao'][i]), 
+    str(df_comp['nivel'][i]), str(df_comp['unidade'][i]), str(df_comp['sigla_uni'][i]))
+
+    cursor.execute(add_entry, data_entry)
+
+cnx.commit()
+
+cursor.close()
+cnx.close()
