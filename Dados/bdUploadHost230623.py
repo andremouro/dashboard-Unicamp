9a96deec03adb254bba395d26661362e7b08eb50
andremouro <andremourodangioli@gmail.com>
2023-06-27 10:45:01 -0300

    adjusting script to upload host values to Data Base

--- Dados/bdUploadHost230623.py
+++ Dados/bdUploadHost230623.py
@@ -4,20 +4,20 @@ import pandas as pd
 
 
 #importando o csv com os dados
-df_host = pd.read_csv('dftemp.csv',  sep=';', encoding='latin-1')
-
 
 cnx = mysql.connector.connect(user = 'root', password = '', host = 'localhost', database = 'mydb')
 
 cursor = cnx.cursor()
 
+cursor = cnx.cursor()
+sql = "DELETE FROM dashboard_host"
 
-for i in range(0, len(df_host['id'])):
-    add_entry = ("INSERT INTO dashboard_host (id, nome_curto, instituicao, nivel, unidade, sigla_uni, hosp) VALUES (%s, %s, %s, %s, %s, %s, %s)")
-    data_entry = (int(df_host['id'][i]), str(df_host['nome_curto'][i]), str(df_host['instituicao'][i]), 
-    str(df_host['nivel'][i]), str(df_host['unidade'][i]), str(df_host['sigla_uni'][i]), str(df_host['hosp'][i]))
+cursor.execute(sql)
 
-    cursor.execute(add_entry, data_entry)
+cnx.commit()
+
+cursor.execute("LOAD DATA LOCAL INFILE 'dftemp.csv' INTO TABLE dashboard_host CHARACTER SET LATIN1 FIELDS TERMINATED BY ';' IGNORE 1 LINES")
+cnx.commit()
 
 cnx.commit()
 
